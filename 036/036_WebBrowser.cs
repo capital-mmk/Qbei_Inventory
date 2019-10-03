105e1a1fb22c343024c1b62b95b35fea2247a0e4
ALH <ckmarcheno27@gmail.com>
2019-10-03 16:56:45 +0630

    Qbei_Inventory[Night]{36PRインターナショナル}.Edit_Comment

--- 036/036_WebBrowser.cs
+++ 036/036_WebBrowser.cs
@@ -16,6 +16,12 @@ using Microsoft.VisualBasic;
 
 namespace _36PRインターナショナル
 {
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <remark>
+    /// Data Table and Common Function and Field
+    /// </remark>
     public partial class frm036 : Form
     {
         DataTable dt = new DataTable();
@@ -26,12 +32,27 @@ namespace _36PRインターナショナル
         Qbei_Entity entity = new Qbei_Entity();
         int i = 0;
 
+        /// <summary>
+        /// System(Start).
+        /// </summary>
+        /// <remark>
+        /// Continue to testflag process.
+        /// </remark>
         public frm036()
         {
             InitializeComponent();
             testflag();
         }
 
+        /// <summary>
+        /// testflag processing.
+        /// </summary>
+        ///<remark>
+        ///"0,1,2"Flage Number of Check. 
+        ///"0" is Start Process.
+        ///"1" is Processing.
+        ///"2" is End Process.
+        ///</remark>
         private void testflag()
         {
             try
@@ -42,11 +63,23 @@ namespace _36PRインターナショナル
                 qe.flag = 1;
                 DataTable dtflag = fun.SelectFlag(36);
                 int flag = Convert.ToInt32(dtflag.Rows[0]["FlagIsFinished"].ToString());
+
+                /// <summary>
+                /// Flag Number of Check.
+                /// </summary>
+                /// <remark>
+                /// Check to flag is "0" or "1" or "2".
+                /// when flag is 0,Change to flag is 1 and Continue to StartRun Process.
+                /// </remark>
                 if (flag == 0)
                 {
                     fun.ChangeFlag(qe);
                     StartRun();
                 }
+
+                ///<remark>
+                ///when flag is 1,To Continue to StartRun Process.
+                ///</remark>
                 else if (flag == 1)
                 {
                     fun.deleteData(36);
@@ -67,6 +100,12 @@ namespace _36PRインターナショナル
             }
         }
 
+        /// <summary>
+        /// Site and Data Table.
+        /// </summary>
+        /// <remark>
+        /// Inspection and processing to Data and Data Table.
+        /// </remark>
         public void StartRun()
         {
             try
@@ -88,6 +127,12 @@ namespace _36PRインターナショナル
             }
         }
 
+        /// <summary>
+        /// Site of Data.
+        /// </summary>
+        /// <remark>
+        /// Read to Data and Url.
+        /// </remark>
         private void ReadData()
         {
             webBrowser1.ScriptErrorsSuppressed = true;
@@ -98,6 +143,9 @@ namespace _36PRインターナショナル
             webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_Start);
         }
 
+        /// <summary>
+        /// Login of Mall.
+        /// </summary>
         private void webBrowser1_Start(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             try
@@ -132,6 +180,9 @@ namespace _36PRインターナショナル
             }
         }
 
+        /// <summary>
+        /// Check to Login.
+        /// </summary>
         private void webBrowser1_Login(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             try
@@ -166,6 +217,9 @@ namespace _36PRインターナショナル
             }
         }
 
+        /// <summary>
+        /// Inspection of item information at Mall.
+        /// </summary>
         private void webBrowser1_ItemSearch(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             try
@@ -376,6 +430,10 @@ namespace _36PRインターナショナル
             }
         }
 
+        /// <summary>
+        /// Inspection of Instance_NavigateError 
+        /// </summary>
+        /// <param name="StatusCode">Insert to Status of Code from Error Data.</param>
         private void instance_NavigateError(object pDisp, ref object URL, ref object Frame, ref object StatusCode, ref bool Cancel)
         {
             if (StatusCode.ToString().Contains("500"))
