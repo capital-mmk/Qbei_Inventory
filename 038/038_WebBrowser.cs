ae3f09bf97d05ebea43023a99a677636b38070bc
ALH <ckmarcheno27@gmail.com>
2019-10-03 17:05:57 +0630

    Qbei_Inventory[Night]{38フタバ}.Edit_Comment

--- 038/038_WebBrowser.cs
+++ 038/038_WebBrowser.cs
@@ -15,6 +15,12 @@ using System.Text.RegularExpressions;
 
 namespace _38フタバ
 {
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <remark>
+    /// Data Table and Common Function and Field
+    /// </remark>
     public partial class frm038 : Form
     {
         DataTable dt = new DataTable();
@@ -24,12 +30,28 @@ namespace _38フタバ
         DataTable dt038 = new DataTable();
         Qbei_Entity entity = new Qbei_Entity();
         int i = -1;
+
+        /// <summary>
+        /// System(Start).
+        /// </summary>
+        /// <remark>
+        /// Continue to testflag process.
+        /// </remark>
         public frm038()
         {
             InitializeComponent();
             testflag();
         }
 
+        /// <summary>
+        /// testflag processing.
+        /// </summary>
+        ///<remark>
+        ///"0,1,2"Flage Number of Check. 
+        ///"0" is Start Process.
+        ///"1" is Processing.
+        ///"2" is End Process.
+        ///</remark>
         private void testflag()
         {
             try
@@ -39,11 +61,24 @@ namespace _38フタバ
                 qe.flag = 0;
                 DataTable dtflag = fun.SelectFlag(38);
                 int flag = Convert.ToInt32(dtflag.Rows[0]["FlagIsFinished"].ToString());
+
+                /// <summary>
+                /// Flag Number of Check.
+                /// </summary>
+                /// <remark>
+                /// Check to flag is "0" or "1" or "2".
+                /// when flag is 0,Change to flag is 1 and Continue to StartRun Process.
+                /// </remark>
                 if (flag == 0)
                 {
                     fun.ChangeFlag(qe);
                     StartRun();
                 }
+
+
+                ///<remark>
+                ///when flag is 1,To Continue to StartRun Process.
+                ///</remark>
                 else if (flag == 1)
                 {
                     fun.deleteData(38);
@@ -64,6 +99,12 @@ namespace _38フタバ
             }
         }
 
+        /// <summary>
+        /// Site and Data Table.
+        /// </summary>
+        /// <remark>
+        /// Inspection and processing to Data and Data Table.
+        /// </remark>
         public void StartRun()
         {
             try
@@ -85,6 +126,12 @@ namespace _38フタバ
             }
         }
 
+        /// <summary>
+        /// Site of Data.
+        /// </summary>
+        /// <remark>
+        /// Read to Data and Url.
+        /// </remark>
         private void ReadData()
         {
             qe.SiteID = 38;
@@ -96,6 +143,10 @@ namespace _38フタバ
 
             webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_Start);
         }
+
+        /// <summary>
+        /// Login of Mall.
+        /// </summary>
         private void webBrowser1_Start(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             try
@@ -128,6 +179,10 @@ namespace _38フタバ
                 Environment.Exit(0);
             }
         }
+
+        /// <summary>
+        /// Check to Login.
+        /// </summary>
         private void webBrowser1_Login(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             try
@@ -160,6 +215,10 @@ namespace _38フタバ
                 Environment.Exit(0);
             }
         }
+
+        /// <summary>
+        /// Wait For Search Page Process.
+        /// </summary>
         private void webBrowser1_WaitForSearchPage(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             webBrowser1.ScriptErrorsSuppressed = true;
@@ -171,6 +230,11 @@ namespace _38フタバ
                 webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_Search);
             }
         }
+
+
+        /// <summary>
+        /// Inspection of item at Mall.
+        /// </summary>
         private void webBrowser1_Search(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             try
@@ -208,6 +272,10 @@ namespace _38フタバ
             }
         }
 
+
+        /// <summary>
+        /// Inspection of item information at Mall.
+        /// </summary>
         private void webBrowser1_ItemProcessing(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             fun.ClearMemory();
@@ -406,6 +474,11 @@ namespace _38フタバ
             webBrowser1.ScriptErrorsSuppressed = true;
             webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_Search);
         }
+
+        /// <summary>
+        /// Inspection of Instance_NavigateError 
+        /// </summary>
+        /// <param name="StatusCode">Insert to Status of Code from Error Data.</param>
         private void instance_NavigateError(object pDisp, ref object URL, ref object Frame, ref object StatusCode, ref bool Cancel)
         {
             string janCode = dt038.Rows[i]["JANコード"].ToString();
