08b8017bba54a2c6bd2f7808815e02ca4fc7b32e
ALH <ckmarcheno27@gmail.com>
2020-04-28 17:05:22 +0630

    Siteid-37{Night}{Edit Logic for Once a Week}

--- 037_Chrome/037.cs
+++ 037_Chrome/037.cs
@@ -172,7 +172,7 @@ namespace _037
                 }
 
                 DataTable dt037 = fun.GetDatatable("037");
-                dt037 = fun.GetOrderData(dt037, "https://ec.tsss.co.jp/aec/user/csv_export_download_info?schedule_id=csv_export_49c35718d482aecec14e92168a955b66", "037", "");
+               // dt037 = fun.GetOrderData(dt037, "https://ec.tsss.co.jp/aec/user/csv_export_download_info?schedule_id=csv_export_49c35718d482aecec14e92168a955b66", "037", "");//<remark Close Logic 2020/04/28 />
                 fun.GetTotalCount("037");
                 string[] str = { "在庫数量", "商品コード", "下代単価" };
                 DataTable dtItem = fun.GetDatatableFromDownloadPath(@"C:\Qbei_Log\037_Download\", str);
