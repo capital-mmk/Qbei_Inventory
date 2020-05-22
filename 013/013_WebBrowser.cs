f5b1bc0638dbd54317aed9b2b6587ef10733670e
ALH <ckmarcheno27@gmail.com>
2020-05-22 16:15:23 +0630

    Siteid-13{Night}{CLose Logic for Qbei Table}

--- 013/013_WebBrowser.cs
+++ 013/013_WebBrowser.cs
@@ -546,21 +546,22 @@ namespace _13ミズタニ
                         //2018-08-14 End
                         entity.stockDate = entity.stockDate.Replace("/","-");
                         //2018/1/12
+                        //<remark Close Logic 2020/25/22 Start>
+                        //if ((dt013.Rows[i]["在庫情報"].ToString().Contains("empty") || dt013.Rows[i]["在庫情報"].ToString().Contains("inquriry")) && dt013.Rows[i]["入荷予定"].ToString().Contains("2100-01-10"))
+                        //{
+                        //    if ((entity.qtyStatus.Contains("empty") && (entity.stockDate.Contains("2100-01-01") || entity.stockDate.Contains("2100-02-01"))) || entity.qtyStatus.Contains("inquiry"))
+                        //    {
+                        //        entity.qtyStatus = dt013.Rows[i]["在庫情報"].ToString();
+                        //        entity.price = dt013.Rows[i]["下代"].ToString();       
+                        //        entity.stockDate = dt013.Rows[i]["入荷予定"].ToString();
+                        //    }
+                        //    fun.Qbei_Inserts(entity);
+                        //}
 
-                        if ((dt013.Rows[i]["在庫情報"].ToString().Contains("empty") || dt013.Rows[i]["在庫情報"].ToString().Contains("inquriry")) && dt013.Rows[i]["入荷予定"].ToString().Contains("2100-01-10"))
-                        {
-                            if ((entity.qtyStatus.Contains("empty") && (entity.stockDate.Contains("2100-01-01") || entity.stockDate.Contains("2100-02-01"))) || entity.qtyStatus.Contains("inquiry"))
-                            {
-                                entity.qtyStatus = dt013.Rows[i]["在庫情報"].ToString();
-                                entity.price = dt013.Rows[i]["下代"].ToString();       
-                                entity.stockDate = dt013.Rows[i]["入荷予定"].ToString();
-                            }
-                            fun.Qbei_Inserts(entity);
-                        }
-
-                        else
-                            //2018/1/12
-                            fun.Qbei_Inserts(entity);
+                        //else
+                        //</reamark 2020/25/22 End>
+                        //2018/1/12
+                        fun.Qbei_Inserts(entity);
                     }
                 }
                 catch (Exception ex)
