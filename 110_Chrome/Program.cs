36c319132d97081e6ba582177eb19876e4b3bcf9
ALH27 <ckmarcheno27@gmail.com>
2021-05-24 11:33:19 +0630

    Siteid-110[Night][Add Logic for Chrome Path]

--- 110_Chrome/Program.cs
+++ 110_Chrome/Program.cs
@@ -121,6 +121,7 @@ namespace _110_Chrome
                 try
                 {
                     var chromeOptions = new ChromeOptions();
+                    chromeOptions.BinaryLocation = @"C:\Program Files\Google\Chrome\Application\chrome.exe";//<Add Logic for Chrome Path 2021/05/24 />
                     using (IWebDriver chrome = new ChromeDriver(chromeOptions))
                     {
                         DataTable dt = new DataTable();
