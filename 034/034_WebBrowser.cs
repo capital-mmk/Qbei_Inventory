ec36c2c262919ad411c4bd737902874649d47bd1
ALH27 <ckmarcheno27@gmail.com>
2022-12-06 11:36:58 +0630

    Siteid-34[Night][Edit Logic for ×在庫限り]

--- 034/034_WebBrowser.cs
+++ 034/034_WebBrowser.cs
@@ -374,7 +374,8 @@ namespace _34シマノ
                                 //else
                                 //{
                                 //<remak Add & Edit Logic for check to ×在庫なし  2022/10/19 Start>
-                                if (alt.Equals("×在庫なし"))
+                                //if (alt.Equals("×在庫なし"))//<remark Edit Logic for check to ×在庫限り 2022/12/06 />
+                                if (alt.Equals("×在庫限り"))
                                 {
                                     entity.stockDate = "2100-02-01";
                                     entity.True_StockDate = date;
